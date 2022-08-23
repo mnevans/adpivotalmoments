3ccd8e283f1cea0ecad5882428b076b482e6fc36
Morgan Evans <moevans@Morgans-MacBook-Pro-2.local>
2022-08-23 15:09:57 -0600

    styling

--- js/style.js
+++ js/style.js
@@ -1,67 +1,8 @@
-// var scrolly = d3.select("#scrolly__section");
-// var chart = scrolly.select(".scrolly__chart");
-// var content = scrolly.select(".scrolly__content");
-// var step = content.selectAll(".step");
-//
-// // initialize the scrollama
-// var scroller = scrollama();
-//
-// // generic window resize listener event
-// function handleResize() {
-//   // 1. update height of step elements
-//   var stepH = Math.floor(window.innerHeight * 1);
-//   step.style("height", stepH + "px");
-//
-//   var figureHeight = window.innerHeight * 0.75;
-//   var figureMarginTop = (window.innerHeight - figureHeight) / 2;
-//
-//   chart
-//     .style("height", figureHeight + "px")
-//     .style("top", figureMarginTop + "px");
-//
-//   // 3. tell scrollama to update new element dimensions
-//   scroller.resize();
-// }
-//
-// // scrollama event handlers
-// function handleStepEnter(response) {
-//   const textblock = step.select(".text-block");
-//
-//   // add color to current step only
-//   textblock.classed("is-active", function(d, i) {
-//     return i === response.index;
-//   });
-//
-//   // update graphic based on step
-//   const linkHead = 'https://flo.uri.sh/story/872914/embed#slide-'
-//   const slide = response.index
-//
-//   d3.select('.scrolly__chart iframe')
-//     .attr('src', linkHead + slide);
-// }
-//
-// function setupStickyfill() {
-//   d3.selectAll(".sticky").each(function() {
-//     Stickyfill.add(this);
-//   });
-// }
-//
-// function init() {
-//   setupStickyfill();
-//   handleResize();
-//   scroller
-//     .setup({
-//       step: "#scrolly__section .scrolly__content .step",
-//       offset: 0.7,
-//       debug: true
-//     })
-//     .onStepEnter(handleStepEnter);
-//
-//   // setup resize event
-//   window.addEventListener("resize", handleResize);
-// }
-//
-// init();
+/*!
+ * Start Bootstrap - Creative Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
 
 (function($) {
     "use strict"; // Start of use strict
